2509e5d2880a141e68da944075a2ba9247ce0455
Hamza <hamza.amghar@student.ehb.be>
2023-12-13 13:01:07 +0100

    Login Update

--- pages/Auth.tsx
+++ pages/Auth.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import { supabase } from "../lib/supabase";
-import { Button, Input } from "react-native-elements";
+import { Button, Input, CheckBox } from "react-native-elements";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
 
-  async function signInWithEmail() {
+  const signInWithEmail = async () => {
     setLoading(true);
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
@@ -17,9 +18,9 @@ export default function Auth() {
 
     if (error) Alert.alert(error.message);
     setLoading(false);
-  }
+  };
 
-  async function signUpWithEmail() {
+  const signUpWithEmail = async () => {
     setLoading(true);
     const {
       data: { session },
@@ -33,43 +34,49 @@ export default function Auth() {
     if (!session)
       Alert.alert("Please check your inbox for email verification!");
     setLoading(false);
-  }
+  };
+
+  const toggleRememberMe = () => {
+    setRememberMe(!rememberMe);
+  };
 
   return (
     <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
+      <View style={styles.formContainer}>
         <Input
           label="Email"
           leftIcon={{ type: "font-awesome", name: "envelope" }}
           onChangeText={(text) => setEmail(text)}
           value={email}
           placeholder="email@address.com"
-          autoCapitalize={"none"}
+          autoCapitalize="none"
         />
-      </View>
-      <View style={styles.verticallySpaced}>
         <Input
           label="Password"
           leftIcon={{ type: "font-awesome", name: "lock" }}
           onChangeText={(text) => setPassword(text)}
           value={password}
-          secureTextEntry={true}
+          secureTextEntry
           placeholder="Password"
-          autoCapitalize={"none"}
+          autoCapitalize="none"
         />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <CheckBox
+          title="Remember Me"
+          checked={rememberMe}
+          onPress={toggleRememberMe}
+          containerStyle={styles.checkBoxContainer}
+        />
         <Button
           title="Sign in"
           disabled={loading}
-          onPress={() => signInWithEmail()}
+          onPress={signInWithEmail}
+          buttonStyle={styles.signInButton}
         />
-      </View>
-      <View style={styles.verticallySpaced}>
         <Button
-          title="Sign up"
+          title="Create Account"
           disabled={loading}
-          onPress={() => signUpWithEmail()}
+          onPress={signUpWithEmail}
+          buttonStyle={styles.signUpButton}
         />
       </View>
     </View>
@@ -78,15 +85,25 @@ export default function Auth() {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 40,
-    padding: 12,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: "stretch",
+  formContainer: {
+    width: "80%",
   },
-  mt20: {
+  checkBoxContainer: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+    margin: 0,
+    padding: 0,
+  },
+  signInButton: {
     marginTop: 20,
+    backgroundColor: "#2196F3", // Green color (you can customize)
+  },
+  signUpButton: {
+    marginTop: 10,
+    backgroundColor: "#2196F3", // Blue color (you can customize)
   },
 });
