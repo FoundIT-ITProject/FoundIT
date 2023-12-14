57b956ce98e1579c0887c485a1c3c0b3d5c82020
zakariyaben10 <133095930+ZakariyaBen10@users.noreply.github.com>
2023-12-14 11:35:22 +0100

    Show users,owner,admin
    It show the list of all the user,owner and admin to manage them

--- pages/admin/users/Admin.tsx
+++ pages/admin/users/Admin.tsx
@@ -1,12 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, FlatList, View, StyleSheet } from "react-native";
+import { supabase } from "../../../lib/supabase";
+import { User } from "@supabase/supabase-js";
 
-const AdminPage: React.FC = () => {
+const OwnerPage = () => {
+  const [userEmails, setUserEmails] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Fetch user emails from Supabase when the component mounts
+    const fetchUserEmails = async () => {
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("role", "Admin"); // Filter by role "User"
+
+      if (error) {
+        console.error("Error fetching user emails:", error.message);
+      } else {
+        // Extract emails from the fetched data
+        const emails = data?.map((value) => value.email) || [];
+        setUserEmails(emails);
+      }
+    };
+
+    fetchUserEmails();
+  }, []);
+
   return (
-    <View>
-      <Text>Admin Page</Text>
+    <View style={styles.cardContainer}>
+      <FlatList
+        data={userEmails}
+        keyExtractor={(email) => email}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text>{item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
 
-export default AdminPage;
+const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+});
+
+export default OwnerPage;
--- pages/admin/users/Regular.tsx
+++ pages/admin/users/Regular.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, FlatList, View } from "react-native";
+import { Text, FlatList, View, StyleSheet } from "react-native";
 import { supabase } from "../../../lib/supabase";
 import { User } from "@supabase/supabase-js";
 
@@ -10,8 +10,9 @@ const RegularPage = () => {
     // Fetch user emails from Supabase when the component mounts
     const fetchUserEmails = async () => {
       const { data, error } = await supabase
-        .from("auth.users")
-        .select("email");
+        .from("users")
+        .select("*")
+        .eq("role", "User"); // Filter by role "User"
 
       if (error) {
         console.error("Error fetching user emails:", error.message);
@@ -26,19 +27,33 @@ const RegularPage = () => {
   }, []);
 
   return (
-    <View>
-      <Text>List of User Emails:</Text>
+    <View style={styles.cardContainer}>
       <FlatList
         data={userEmails}
         keyExtractor={(email) => email}
         renderItem={({ item }) => (
-          <View>
-            <Text>Email: {item}</Text>
+          <View style={styles.card}>
+            <Text>{item}</Text>
           </View>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+});
 
 export default RegularPage;
