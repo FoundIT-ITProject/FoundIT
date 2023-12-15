8b091fb17157e3cb571c9bacac8c4f9a4e13383e
Mehdi Merkachi <merkachimehdi@gmail.com>
2023-12-15 17:46:03 +0100

    Fix Error: Modal won't open due to react navigation & typescript error, now works

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,13 +1,26 @@
 import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 import { FIREBASE_AUTH } from "../lib/firebaseConfig";
+import { useNavigation } from "@react-navigation/native";
+import CreateItemButton from "../components/CreateItemButton";
+type RootStackParamList = {
+  Home: undefined;
+  UploadItem: undefined;
+  // Add other screens here
+};
 
 const Home = () => {
+  type NavigationProp = NativeStackNavigationProp<RootStackParamList, "Home">;
+
   const currentUser = FIREBASE_AUTH.currentUser;
+  const navigation = useNavigation<NavigationProp>();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Wlcome {currentUser?.displayName}</Text>
+      <Text>Welcome {currentUser?.displayName}</Text>
+      <CreateItemButton onPress={() => navigation.navigate("UploadItem")} />
       <TouchableOpacity
         style={styles.SignoutButton}
         onPress={() => {
