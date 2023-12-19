c73b25a6fef5d529c8b7b299d6dd65739cb2a1b2
Hamza-180 <hamza.amghar@student.ehb.be>
2023-12-19 16:16:15 +0100

    admin page and owner
    i make a admin page  only for admin
    
    and i make a owner page only for owner
    and i make some change on app.tsx

---
+++ pages/OwnerPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const OwnerPage = () => {
+  return (
+    <View>
+      <Text>Welcome to the Owner Page!</Text>
+    </View>
+  );
+};
+
+export default OwnerPage;
